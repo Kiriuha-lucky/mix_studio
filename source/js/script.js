c51854275658cd8bd6eb34ac700aa12e97ab8d3a
Kiriuha-lucky <Kiriuha777@gmail.com>
2021-04-21 22:26:07 +1200

    add some animate

--- source/js/script.js
+++ source/js/script.js
@@ -24,13 +24,56 @@ jQuery(document).ready(function () {
     }
   });
 
-  $('.owl-carousel').owlCarousel({
+  $('#owl-carousel').owlCarousel({
     loop: true,
     autoplay: false,
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
-    dots: false
+    dots: false,
+    center: true,
+    autoWidth: true,
+    responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+      0:{
+          items:2,
+          margin: 20
+      },
+      768:{
+          items:2,
+          margin: 30
+      },
+      1290:{
+          items:6,
+          margin: 30
+      }
+  }
   });
+
+  $('#owl-carousel2').owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    dots: false,
+    center: true,
+    autoWidth: true,
+    nav: false,
+    responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+      0:{
+          items:3,
+          margin: 20
+      },
+      768:{
+          items:2,
+          margin: 30
+      },
+      1290:{
+          items:3,
+          margin: 30
+      }
+  }
+  });
+
+
 
 
 });
