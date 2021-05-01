d13eb423dbde96d12e51d7f0ab4aa6b7c89a49ab
Kiriuha-lucky <Kiriuha777@gmail.com>
2021-05-01 15:54:42 +1200

    add

--- source/js/script.js
+++ source/js/script.js
@@ -211,7 +211,20 @@ jQuery(document).ready(function () {
   })();
 
 
-
+  (function () {
+    var s = document['createElement']('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.charset = 'utf-8';
+    s.src = '//cleversite.ru/cleversite/widget_new.php?supercode=1&referer_main=' + encodeURIComponent(document
+      .referrer) + '&clid=100242SyhkZ&siteNew=127995';
+    var ss = document['getElementsByTagName']('script')[0];
+    if (ss) {
+      ss.parentNode.insertBefore(s, ss);
+    } else {
+      document.documentElement.firstChild.appendChild(s);
+    };
+  })()
 
 
 });
